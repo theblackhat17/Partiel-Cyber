90e294552a94125b1bb63b2a4ca43255107bcc4c
Clément Vandewalle <tbhone@MacBook-Pro-de-Clement.local>
2024-12-05 10:26:51 +0100

    Exercice 1 - Hardening

---
+++ ex1.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+
+def block_usb_ports():
+    if os.name == 'posix':
+        subprocess.run(['sudo', 'modprobe', '-r', 'usb_storage'], check=True)
+
+def configure_firewall():
+    if os.name == 'posix':
+        subprocess.run(['sudo', 'iptables', '-P', 'INPUT', 'DROP'], check=True)
+        subprocess.run(['sudo', 'iptables', '-P', 'OUTPUT', 'DROP'], check=True)
+        subprocess.run(['sudo', 'iptables', '-P', 'FORWARD', 'DROP'], check=True)
+
+def reset_firewall():
+    if os.name == 'posix':
+        subprocess.run(['sudo', 'iptables', '-P', 'INPUT', 'ACCEPT'], check=True)
+        subprocess.run(['sudo', 'iptables', '-P', 'OUTPUT', 'ACCEPT'], check=True)
+        subprocess.run(['sudo', 'iptables', '-P', 'FORWARD', 'ACCEPT'], check=True)
+
+if __name__ == '__main__':
+    block_usb_ports()
+    configure_firewall()
